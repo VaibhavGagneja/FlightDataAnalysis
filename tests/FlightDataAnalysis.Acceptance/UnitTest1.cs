d7e7818f2299dd022aac4d4da5b5a9e3c84051d8
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-02-24 00:27:40 +0200

    initialized the project

---
+++ tests/FlightDataAnalysis.Acceptance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FlightDataAnalysis.Acceptance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/FlightDataAnalysis.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FlightDataAnalysis.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
